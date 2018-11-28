d12b0c463d9cc2f802dd66cde5509dd08a3bdb58
Bastian Blank <waldi@debian.org>
2018-11-28 15:50:53 +0100

    Generate tar including a disk image for tests

--- tests/debian_cloud_images/test_images.py
+++ tests/debian_cloud_images/test_images.py
@@ -1,3 +1,4 @@
+import io
 import json
 import pathlib
 import pkg_resources
@@ -38,6 +39,28 @@ def images_path(tmp_path):
     return tmp_path
 
 
+@pytest.fixture
+def images_path_tar(images_path):
+    with images_path.joinpath('test.tar').open('wb') as f:
+        with tarfile.open(fileobj=f, mode='x:') as tar:
+            info = tarfile.TarInfo(name='disk.raw')
+            info.size = 1024 * 1024
+            tar.addfile(info, io.BytesIO(b'1' * 1024 * 1024))
+
+    return images_path
+
+
+@pytest.fixture
+def images_path_tar_xz(images_path):
+    with images_path.joinpath('test.tar.xz').open('wb') as f:
+        with tarfile.open(fileobj=f, mode='x:xz') as tar:
+            info = tarfile.TarInfo(name='disk.raw')
+            info.size = 1024 * 1024
+            tar.addfile(info, io.BytesIO(b'1' * 1024 * 1024))
+
+    return images_path
+
+
 def test_Images(images_path):
     images = Images()
     images.read_path(images_path)
@@ -54,21 +77,15 @@ def test_Image(images_path):
         image.get_tar()
 
 
-def test_Image_get_tar(images_path):
-    with tarfile.open(images_path.joinpath('test.tar').as_posix(), 'w:'):
-        pass
-
+def test_Image_get_tar(images_path_tar):
     images = Images()
-    images.read_path(images_path)
+    images.read_path(images_path_tar)
     image = images['test']
     assert image.get_tar()
 
 
-def test_Image_get_tar_xz(images_path):
-    with tarfile.open(images_path.joinpath('test.tar.xz').as_posix(), 'w:xz'):
-        pass
-
+def test_Image_get_tar_xz(images_path_tar_xz):
     images = Images()
-    images.read_path(images_path)
+    images.read_path(images_path_tar_xz)
     image = images['test']
     assert image.get_tar()
