ca78ff9029ecbee36286ae43c8365eb4c03f1c29
Bastian Blank <waldi@debian.org>
2019-04-25 13:53:28 +0200

    Clean contents of /var/log

---
+++ system_tests/image/test_var.py
@@ -0,0 +1,13 @@
+import pathlib
+
+
+class TestVar:
+    def test_var_log(self, image_path):
+        p = image_path / 'var' / 'log'
+        assert p.exists(), '/var/log does not exist'
+        assert p.is_dir(), '/var/log is no directory'
+
+        c = set(i.relative_to(p).as_posix() for i in p.glob('*'))
+        c.difference_update(('apt', 'btmp', 'chrony', 'faillog', 'lastlog', 'unattended-upgrades', 'wtmp'))
+
+        assert len(c) == 0, '/var/log contains unexpected files: {}'.format(', '.join(c))
