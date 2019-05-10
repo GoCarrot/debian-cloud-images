194de79a3c1e1f0702660e01def45cd4dc94190a
Noah Meyerhans <noahm@debian.org>
2019-05-09 21:06:03 -0700

    tests: ensure that critical boot files exist
    Confirm that a grub config, initrd, and kernel image are present

--- system_tests/image/test_boot.py
+++ system_tests/image/test_boot.py
@@ -1,5 +1,16 @@
 import pytest
+import pathlib
 
+class TestBootFiles:
+    @pytest.mark.parametrize('path', [
+        '/boot/initrd.img*',
+        '/boot/vmlinu[xz]*',
+        '/boot/grub/grub.cfg',
+    ])
+    def test_boot_files(self, image_path, path):
+        path = pathlib.Path(path)
+        p = (image_path / path.relative_to('/'))
+        assert len(list(p.parent.glob(p.name))) > 0, 'No files matching {}'.format(path)
 
 class TestBootGce:
     @pytest.fixture(scope="class", autouse=True)
