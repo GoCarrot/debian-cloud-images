50cd1b22d977be9b5c6ec270acc92dc0d73c0a77
Bastian Blank <waldi@debian.org>
2019-07-03 19:20:55 +0200

    Define type specific Azure offer and sku

--- src/debian_cloud_images/images/publicinfo.py
+++ src/debian_cloud_images/images/publicinfo.py
@@ -4,16 +4,22 @@ import enum
 @enum.unique
 class ImagePublicType(enum.Enum):
     dev = {
+        'azure_offer': 'debian-test',
+        'azure_sku': '{release_id}',
         'path': '{release}/dev/{build_id}/debian-{release_id}-{vendor}-{arch}-dev-{build_id}-{version}',
         'vendor_family': 'debian-{release_id}-{arch}-dev-{build_id}',
         'vendor_description': 'Debian {release_id} (development build {build_id}-{version})',
     }
     daily = {
+        'azure_offer': 'debian-{release_baseid}-daily',
+        'azure_sku': '{release_id}',
         'path': '{release}/daily/{version}/debian-{release_id}-{vendor}-{arch}-daily-{version}',
         'vendor_family': 'debian-{release_id}-{arch}-daily',
         'vendor_description': 'Debian {release_id} (daily build {version})',
     }
     release = {
+        'azure_offer': 'debian-{release_baseid}',
+        'azure_sku': '{release_id}',
         'path': '{release}/{version}/debian-{release_id}-{vendor}-{arch}-{version}',
         'vendor_family': 'debian-{release_id}-{arch}',
         'vendor_description': 'Debian {release_id} ({version})',
