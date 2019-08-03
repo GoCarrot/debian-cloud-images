1453d384443c0759a9081e2b94fa633b5ebc6706
Bastian Blank <waldi@debian.org>
2019-08-03 23:17:41 +0200

    Add support for hash arguments

--- tests/debian_cloud_images/utils/test_argparse_ext.py
+++ tests/debian_cloud_images/utils/test_argparse_ext.py
@@ -1,7 +1,7 @@
 import argparse
 import os
 
-from debian_cloud_images.utils.argparse_ext import ActionEnv
+from debian_cloud_images.utils.argparse_ext import ActionEnv, ConfigHashAction
 
 
 def test_ActionEnv():
@@ -16,3 +16,22 @@ def test_ActionEnv():
     assert args.home == 'test'
     args = parser.parse_args([])
     assert args.home == os.environ['HOME']
+
+
+def test_ConfigHashAction():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--test',
+        action=ConfigHashAction,
+        config={'test': 'default1=value1 default2=value2'},
+        config_key='test',
+        nargs='+',
+    )
+    args = parser.parse_args(['--test', 'key1=value1'])
+    assert args.test == {'key1': 'value1'}
+    args = parser.parse_args(['--test', 'key1=value1', 'key2=value2'])
+    assert args.test == {'key1': 'value1', 'key2': 'value2'}
+    args = parser.parse_args(['--test', 'key1=value1', '--test', 'key2=value2'])
+    assert args.test == {'key1': 'value1', 'key2': 'value2'}
+    args = parser.parse_args([])
+    assert args.test == {'default1': 'value1', 'default2': 'value2'}
