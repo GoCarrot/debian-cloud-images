c51e6edaa623c16e2278c536832f7c7fc6b3b018
Bastian Blank <waldi@debian.org>
2019-09-02 14:34:18 +0200

    Don't remove files from qemu-utils

--- system_tests/image/test_bin.py
+++ system_tests/image/test_bin.py
@@ -1,3 +1,5 @@
 class TestBin:
     def test_bin_qemu(self, image_path):
-        assert len(list((image_path / 'usr/bin').glob('qemu-*'))) == 0
+        exist = frozenset(i.name for i in (image_path / 'usr/bin').glob('qemu-*'))
+        ignore = frozenset(('qemu-img', 'qemu-io', 'qemu-nbd'))
+        assert len(exist - ignore) == 0
