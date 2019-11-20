4e936047f5f15d9e86a983e838ba584eef3bf1ef
Zach Marano <zmarano@google.com>
2019-11-20 21:27:51 +0000

    Remove gsetup files from GCE images.

--- system_tests/image/test_boot.py
+++ system_tests/image/test_boot.py
@@ -12,13 +12,3 @@ class TestBootFiles:
         path = pathlib.Path(path)
         p = (image_path / path.relative_to('/'))
         assert len(list(p.parent.glob(p.name))) > 0, 'No files matching {}'.format(path)
-
-
-class TestBootGce:
-    @pytest.fixture(scope="class", autouse=True)
-    def check_vendor(self, image_build_info):
-        if image_build_info['vendor'] != 'gce':
-            pytest.skip('Image vendor is not gce')
-
-    def test_boot_efi_google(self, image_path):
-        assert (image_path / 'boot/efi/EFI/Google/gsetup').exists()
