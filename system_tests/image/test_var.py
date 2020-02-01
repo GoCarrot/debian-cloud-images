a1ac3da63b596ffbde053b326a97107a0286d9b8
Bastian Blank <waldi@debian.org>
2020-02-01 18:05:07 +0100

    Re-format list to be better readable

--- system_tests/image/test_var.py
+++ system_tests/image/test_var.py
@@ -5,6 +5,15 @@ class TestVar:
         assert p.is_dir(), '/var/log is no directory'
 
         c = set(i.relative_to(p).as_posix() for i in p.glob('*'))
-        c.difference_update(('apt', 'btmp', 'chrony', 'faillog', 'lastlog', 'runit', 'unattended-upgrades', 'wtmp'))
+        c.difference_update((
+            'apt',
+            'btmp',
+            'chrony',
+            'faillog',
+            'lastlog',
+            'runit',
+            'unattended-upgrades',
+            'wtmp',
+        ))
 
         assert len(c) == 0, '/var/log contains unexpected files: {}'.format(', '.join(c))
