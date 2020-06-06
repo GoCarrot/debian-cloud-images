2fca62daa29455ecb833f3a56b385904e7b178f8
Bastian Blank <waldi@debian.org>
2020-06-06 18:36:18 +0200

    Record digest of public images

--- src/debian_cloud_images/images/public/image.py
+++ src/debian_cloud_images/images/public/image.py
@@ -1,11 +1,12 @@
+import base64
+import hashlib
 import json
 import logging
 import pathlib
-import shutil
 
 from ...api.cdo.upload import Upload
 from ...api.registry import registry as api_registry
-from ...api.wellknown import label_ucdo_image_format, label_ucdo_type
+from ...api.wellknown import annotation_cdo_digest, label_ucdo_image_format, label_ucdo_type
 
 
 logger = logging.getLogger(__name__)
@@ -67,23 +68,44 @@ class Image:
             path = self.__path.with_suffix('.qcow2')
             ref = self.__ref + '.qcow2'
             logger.info(f'Copy to {ref}')
-            with path.open('wb') as f:
-                shutil.copyfileobj(f_in, f)
+            with path.open('wb') as f_out:
+                output_hash = self.__copy_hash(f_in, f_out)
 
-        self._append_manifest(image, public_type, ref, 'qcow2')
+        self._append_manifest(image, public_type, ref, 'qcow2', output_hash)
 
     def _copy_tar(self, image, public_type):
         with image.open_tar_raw() as f_in:
             path = self.__path.with_suffix(f_in.extension)
             ref = self.__ref + f_in.extension
             logger.info(f'Copy to {ref}')
-            with path.open('wb') as f:
-                shutil.copyfileobj(f_in, f)
+            with path.open('wb') as f_out:
+                output_hash = self.__copy_hash(f_in, f_out)
 
-        self._append_manifest(image, public_type, ref, 'internal')
+        self._append_manifest(image, public_type, ref, 'internal', output_hash)
 
-    def _append_manifest(self, image, public_type, ref, image_format):
+    def __copy_hash(self, f_in, f_out, length=64 * 1024):
+        output_hash = hashlib.sha512()
+
+        with memoryview(bytearray(length)) as mv:
+            while True:
+                n = f_in.readinto(mv)
+                if not n:
+                    break
+                elif n < length:
+                    with mv[:n] as smv:
+                        f_out.write(smv)
+                        output_hash.update(smv)
+                else:
+                    f_out.write(mv)
+                    output_hash.update(mv)
+
+        return output_hash
+
+    def _append_manifest(self, image, public_type, ref, image_format, output_hash):
+        output_digest = base64.b64encode(output_hash.digest()).decode().rstrip('=')
+
         metadata = image.build.metadata.copy()
+        metadata.annotations[annotation_cdo_digest] = f'{output_hash.name}:{output_digest}'
         metadata.labels[label_ucdo_image_format] = image_format
         metadata.labels[label_ucdo_type] = public_type
 
