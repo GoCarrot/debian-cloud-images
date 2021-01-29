8c35965374774b0c389ecd6d0b8294ac1915f6ba
Bastian Blank <waldi@debian.org>
2021-01-29 16:25:02 +0100

    Add support for Azure auth via az tool

--- src/debian_cloud_images/utils/libcloud/common/azure.py
+++ src/debian_cloud_images/utils/libcloud/common/azure.py
@@ -1,4 +1,12 @@
+import datetime
+import subprocess
+import json
+import logging
+
 from libcloud.common.azure_arm import AzureResourceManagementConnection
+
+
+logger = logging.getLogger(__name__)
 
 
 class AzureGenericOAuth2Connection(AzureResourceManagementConnection):
@@ -8,3 +16,21 @@ class AzureGenericOAuth2Connection(AzureResourceManagementConnection):
         self.host = host
         self.tenant_id = tenant_id
         self.login_resource = login_resource
+
+    def get_token_from_credentials(self):
+        if self.user_id and self.key:
+            return super().get_token_from_credentials()
+
+        logger.debug('Trying to get auth via az account get-access-token')
+        data = json.loads(subprocess.check_output((
+            'az',
+            'account',
+            'get-access-token',
+            f'--tenant={self.tenant_id}',
+            f'--resource={self.login_resource}',
+            '--output=json',
+        )))
+        assert data['tokenType'] == 'Bearer'
+
+        self.access_token = data['accessToken']
+        self.expires_on = datetime.datetime.fromisoformat(data['expiresOn']).timestamp()
