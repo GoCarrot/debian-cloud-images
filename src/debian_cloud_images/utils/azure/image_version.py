32551d0781d2e37cdb7dc19793f39d7f3cda5acd
Bastian Blank <waldi@debian.org>
2021-02-07 19:46:39 +0100

    Remove not required comparision

--- src/debian_cloud_images/utils/azure/image_version.py
+++ src/debian_cloud_images/utils/azure/image_version.py
@@ -1,8 +1,6 @@
 import collections
-import functools
 
 
-@functools.total_ordering
 class AzureImageVersion(collections.namedtuple('AzureImageVersion', ['major', 'minor', 'patch'])):
     def __new__(cls, major, minor, patch):
         return super().__new__(cls, int(major), int(minor), int(patch))
@@ -10,25 +8,6 @@ class AzureImageVersion(collections.namedtuple('AzureImageVersion', ['major', 'm
     @classmethod
     def from_string(cls, s):
         return cls(*s.split('.'))
-
-    def __eq__(self, other):
-        return (self.major == other.major
-                and self.minor == other.minor   # noqa:W503
-                and self.patch == other.patch)  # noqa:W503
-
-    def __lt__(self, other):
-        if self.major < other.major:
-            return True
-        elif self.major > other.major:
-            return False
-        if self.minor < other.minor:
-            return True
-        elif self.minor > other.minor:
-            return False
-        return self.patch < other.patch
-
-    def __hash__(self):
-        return hash(self.major) + hash(self.minor) + hash(self.patch)
 
     def __str__(self):
         return f'{self.major}.{self.minor}.{self.patch}'
