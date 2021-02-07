068bd7686a46e67287d5289460e922805c1d471e
Bastian Blank <waldi@debian.org>
2021-02-07 19:48:37 +0100

    Add EC2 cleanup support

---
+++ src/debian_cloud_images/cli/cleanup_ec2.py
@@ -0,0 +1,69 @@
+import datetime
+import logging
+
+from .base import BaseCommand
+from ..images.ec2 import Ec2Images
+from ..utils.libcloud.compute.ec2 import ExEC2NodeDriver
+
+
+class CleanupEc2Command(BaseCommand):
+    argparser_name = 'cleanup-ec2'
+    argparser_help = ''
+    argparser_epilog = '''
+config options:
+  ec2.auth.key
+  ec2.auth.secret
+  ec2.image.regions
+'''
+
+    compute_cls = ExEC2NodeDriver
+
+    @classmethod
+    def _argparse_register(cls, parser):
+        super()._argparse_register(parser)
+
+        parser.add_argument(
+            '--delete-after',
+            help='Delete images after X days',
+            metavar='DAYS',
+            type=int,
+        )
+        parser.add_argument(
+            '--no-op',
+            action='store_true',
+        )
+
+    def __init__(
+            self, *,
+            delete_after=None,
+            no_op=False,
+            date_today=datetime.datetime.now(),
+            **kw,
+    ):
+        super().__init__(**kw)
+        self.no_op = no_op
+
+        if delete_after:
+            self.delete_date = date_today - datetime.timedelta(days=delete_after)
+        else:
+            self.delete_date = None
+
+        key = self.config_get('ec2.auth.key')
+        secret = self.config_get('ec2.auth.secret')
+        token = self.config_get('ec2.auth.token', default=None)
+
+        self.drivers_compute = {
+            region.name: self.compute_cls(key=key, secret=secret, token=token, region=region.name)
+            for region in self.compute_cls(key=key, secret=secret, token=token, region='us-east-1').ex_list_regions()
+        }
+
+    def __call__(self):
+        if self.delete_date:
+            logging.info(f'Deleting images before {self.delete_date.strftime("%Y-%m-%d")}')
+            Ec2Images(self.no_op, None, self.drivers_compute, None).cleanup(self.delete_date)
+        else:
+            logging.info('Not deleting images')
+
+
+if __name__ == '__main__':
+    CleanupEc2Command._main()
---
+++ src/debian_cloud_images/images/ec2/s2_cloud_version.py
@@ -0,0 +1,71 @@
+import collections.abc
+import logging
+import typing
+
+from .info import Ec2Info
+from ...utils.image_version import ImageVersion
+
+
+logger = logging.getLogger(__name__)
+
+
+class StepCloudVersion:
+    _info: Ec2Info
+    _name: ImageVersion
+
+    images: dict[str, typing.Any]
+    snapshots: dict[str, typing.Any]
+
+    def __init__(self, info: Ec2Info, name: ImageVersion) -> None:
+        self._info = info
+        self._name = name
+
+        self.images = {}
+        self.snapshots = {}
+
+    def __enter__(self) -> 'StepCloudVersion':
+        return self
+
+    def __exit__(self, type, value, tb) -> None:
+        pass
+
+    def delete(self) -> None:
+        for region, image in sorted(self.images.items()):
+            if not self._info.noop:
+                logger.debug(f'Deleting image {image.name} from region {region}')
+                image.driver.delete_image(image)
+            else:
+                logger.info(f'Would deleting image {image.name} from region {region}')
+
+        for region, snapshot in sorted(self.snapshots.items()):
+            image_name = snapshot.extra['tags']['AMI']
+            if not self._info.noop:
+                logger.debug(f'Deleting snapshot {snapshot.id} for image {image_name} from region {region}')
+                snapshot.driver.destroy_volume_snapshot(snapshot)
+            else:
+                logger.info(f'Would deleting snapshot {snapshot.id} for image {image_name} from region {region}')
+
+
+class StepCloudVersions(collections.abc.Mapping):
+    _info: Ec2Info
+    _children: dict[ImageVersion, StepCloudVersion]
+
+    def __init__(self, info: Ec2Info) -> None:
+        self._info = info
+        self._children = {}
+
+    def __delitem__(self, name: ImageVersion) -> None:
+        self._children[name].delete()
+        del self._children[name]
+
+    def __getitem__(self, name: ImageVersion) -> StepCloudVersion:
+        return self._children[name]
+
+    def __iter__(self) -> typing.Iterator[ImageVersion]:
+        return iter(self._children)
+
+    def __len__(self) -> int:
+        return len(self._children)
+
+    def setdefault(self, name: ImageVersion) -> StepCloudVersion:
+        return self._children.setdefault(name, StepCloudVersion(self._info, name))
