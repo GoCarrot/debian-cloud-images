fa73fcfed6ffece9b11fd2d8250d21dd4d55e9cb
Bastian Blank <waldi@debian.org>
2021-10-14 11:04:56 +0200

    Add release bookworm

--- src/debian_cloud_images/cli/build.py
+++ src/debian_cloud_images/cli/build.py
@@ -119,6 +119,13 @@ ReleaseEnum = enum.Enum(  # type:ignore
             'fai_classes': ('BULLSEYE', 'BACKPORTS_LINUX', 'EXTRAS'),
             'arch_supports_linux_image_cloud': ('amd64', 'arm64',),
         },
+        'bookworm': {
+            'basename': 'bookworm',
+            'id': '12',
+            'baseid': '12',
+            'fai_classes': ('BOOKWORM', 'EXTRAS'),
+            'arch_supports_linux_image_cloud': ('amd64', 'arm64',),
+        },
         'sid': {
             'basename': 'sid',
             'id': 'sid',
