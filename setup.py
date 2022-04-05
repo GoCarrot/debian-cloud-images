391d7443c45a1d5ae1c33587854739792da2eaa6
Bastian Blank <waldi@debian.org>
2022-04-05 21:27:45 +0200

    Run full tests with latest Python and pytest

--- setup.py
+++ setup.py
@@ -3,16 +3,10 @@ import setuptools
 setuptools.setup(
     name='debian_cloud_images',
     version='0',
-    setup_requires=[
-        'pytest-runner',
-    ],
     install_requires=[
         'apache-libcloud',
         'marshmallow>=3.0.0b14',
         'PyYAML',
-    ],
-    tests_require=[
-        'pytest',
     ],
     packages=setuptools.find_namespace_packages(where="src"),
     package_dir={"": "src"},
