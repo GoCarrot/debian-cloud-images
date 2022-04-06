049e6ae8f717bf4b302d6623d4bcd64c31c64b65
Bastian Blank <waldi@debian.org>
2022-04-06 14:43:28 +0200

    Remove all existing Azure Partner support

--- src/debian_cloud_images/cli/__main__.py
+++ src/debian_cloud_images/cli/__main__.py
@@ -3,11 +3,8 @@ import argparse
 from .build import BuildCommand
 from .cleanup import CleanupCommand
 from .cleanup_ec2 import CleanupEc2Command
-from .cleanup_azure_cloudpartner import CleanupAzureCloudpartnerCommand
-from .release_azure_cloudpartner import ReleaseAzureCloudpartnerCommand
 from .upload import UploadCommand
 from .upload_azure import UploadAzureCommand
-from .upload_azure_cloudpartner import UploadAzureCloudpartnerCommand
 from .upload_azure_computegallery import UploadAzureComputegalleryCommand
 from .upload_ec2 import UploadEc2Command
 from .upload_gce import UploadGceCommand
@@ -28,11 +25,8 @@ def main():
     BuildCommand._argparse_init_sub(subparsers)
     CleanupCommand._argparse_init_sub(subparsers)
     CleanupEc2Command._argparse_init_sub(subparsers)
-    CleanupAzureCloudpartnerCommand._argparse_init_sub(subparsers)
-    ReleaseAzureCloudpartnerCommand._argparse_init_sub(subparsers)
     UploadCommand._argparse_init_sub(subparsers)
     UploadAzureCommand._argparse_init_sub(subparsers)
-    UploadAzureCloudpartnerCommand._argparse_init_sub(subparsers)
     UploadAzureComputegalleryCommand._argparse_init_sub(subparsers)
     UploadEc2Command._argparse_init_sub(subparsers)
     UploadGceCommand._argparse_init_sub(subparsers)
--- tests/debian_cloud_images/images/azure_partner/test_.py
+++
@@ -1,1 +0,0 @@
-import debian_cloud_images.images.azure_partner  # noqa:F401
