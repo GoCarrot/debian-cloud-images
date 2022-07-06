GoCarrot/debian-cloud-images

commit 6eb5629911a5f4b37da4ec10badd4c34699d896b

retry EC2 AMI tagging in case of failure
AMI registration is asynchronous, and an AMI ID is not necessarily
usable immediately after the RegisterImages call completes.

Fixes https://salsa.debian.org/cloud-team/debian-cloud-images/-/issues/7

src/debian_cloud_images/cli/upload_ec2.py
@@ -2,12 +2,18 @@ import logging
 import time
 
 from libcloud.compute.types import VolumeSnapshotState
+import libcloud.common.exceptions
 
 from .upload_base import UploadBaseCommand
 from ..api.cdo.upload import Upload
 from ..api.wellknown import label_ucdo_provider, label_ucdo_type
 from ..utils.libcloud.compute.ec2 import ExEC2NodeDriver
 from ..utils.libcloud.storage.s3 import S3BucketStorageDriver
+
+
+class EC2Exception(Exception):
+    def __init__(self, message):
+        self.message = message
 
 
 class ImageUploaderEc2:
@@ -135,12 +141,27 @@ class ImageUploaderEc2:
 
             logging.info('Image %s/%s arch %s registered from %s', driver.region_name, ec2_image.id, architecture, snapshot.id)
 
-            driver.ex_create_tags(ec2_image, self.generate_tags(image, public_info))
-            driver.ex_modify_image_attribute(
-                ec2_image,
-                self.generate_permissions('LaunchPermission'),
-            )
+            found = False
+            tries_remaining = 10
 
+            while not found and tries_remaining > 10:
+                tries_remaining -= 1
+                try:
+                    driver.ex_create_tags(ec2_image, self.generate_tags(image, public_info))
+                    driver.ex_modify_image_attribute(
+                        ec2_image,
+                        self.generate_permissions('LaunchPermission'),
+                    )
+                    found = True
+                except libcloud.common.exceptions.BaseHTTPError as e:
+                    if "InvalidAMIID.NotFound" in e.message:
+                        logging.info("Image %s not yet available in %s.",
+                                     ec2_image.id, driver.region_name)
+                        time.sleep(10)
+                    else:
+                        raise
+            if not found:
+                raise EC2Exception("AMI %s never became available in %s" % (ec2_image.image_id, driver.region))
             ec2_images[driver.region_name] = ec2_image
 
         return ec2_images

commit f87e348b314dd48336f18891f63c98257493e26d

Fix ec2 tagging error handling
Fixes: 6eb5629 ("retry EC2 AMI tagging in case of failure")

src/debian_cloud_images/cli/upload_ec2.py
@@ -142,9 +142,9 @@ class ImageUploaderEc2:
             logging.info('Image %s/%s arch %s registered from %s', driver.region_name, ec2_image.id, architecture, snapshot.id)
 
             found = False
-            tries_remaining = 10
+            tries_remaining = 30
 
-            while not found and tries_remaining > 10:
+            while not found and tries_remaining > 0:
                 tries_remaining -= 1
                 try:
                     driver.ex_create_tags(ec2_image, self.generate_tags(image, public_info))
@@ -161,7 +161,7 @@ class ImageUploaderEc2:
                     else:
                         raise
             if not found:
-                raise EC2Exception("AMI %s never became available in %s" % (ec2_image.image_id, driver.region))
+                raise EC2Exception("AMI %s never became available in %s" % (ec2_image.id, driver.region))
             ec2_images[driver.region_name] = ec2_image
 
         return ec2_images