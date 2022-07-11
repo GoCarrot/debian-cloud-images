259eb261b59bb45118882e9f6e2eddee69afa882
Bastian Blank <waldi@debian.org>
2022-07-11 11:15:21 +0200

    Support offer slots in Azure Partner interface

--- tests/debian_cloud_images/images/azure_partnerlegacy/test_s1_offer.py
+++ tests/debian_cloud_images/images/azure_partnerlegacy/test_s1_offer.py
@@ -10,7 +10,7 @@ class TestImagesAzurePartnerlegacyOffer:
 
     def test_get(self, azure_conn, requests_mock):
         requests_mock.get(
-            'https://host/api/publishers/publisher/offers/offer?api-version=2017-10-31',
+            'https://host/api/publishers/publisher/offers/offer/slot/production?api-version=2017-10-31',
             json=self.data.copy(),
         )
 
@@ -20,7 +20,7 @@ class TestImagesAzurePartnerlegacyOffer:
             azure_conn,
         )
 
-        assert t.get() == self.data
+        assert t.get('production') == self.data
 
     def test_put(self, azure_conn, requests_mock):
         put = requests_mock.put(
@@ -69,7 +69,7 @@ class TestImagesAzurePartnerlegacyOffer:
 
     def test_op_cleanup(self, azure_conn, requests_mock):
         requests_mock.get(
-            'https://host/api/publishers/publisher/offers/offer?api-version=2017-10-31',
+            'https://host/api/publishers/publisher/offers/offer/slot/draft?api-version=2017-10-31',
             json={
                 'definition': {
                     'plans': [
