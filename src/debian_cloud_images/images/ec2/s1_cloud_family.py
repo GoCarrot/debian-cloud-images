GoCarrot/debian-cloud-images

commit 5d252c346891ce50b25d3b8bb59a35b2a81bee7c

ec2: ignore snapshots associated with ignored AMIs
When building an inventory of AMIs and snapshots in a given region, we
skip the processing of AMIs that don't have expected tags.  With this
change, we also ignore any associated snapshots.

Fixes https://salsa.debian.org/cloud-team/debian-cloud-images/-/issues/45

src/debian_cloud_images/images/ec2/s1_cloud_family.py
@@ -53,6 +53,8 @@ class StepCloudFamilies(collections.abc.Mapping):
         for region, driver in sorted(self._info.drivers_compute.items()):
             logger.debug(f'Reading from region {region}')
 
+            ignored_snapshots = []
+
             for image in driver.list_images(ex_owner=self._info.account):
                 tags = image.extra['tags']
                 family = tags.get('ImageFamily', None)
@@ -61,12 +63,18 @@ class StepCloudFamilies(collections.abc.Mapping):
                     v = self.setdefault(family).versions.setdefault(ImageVersion.from_string(version))
                     v.images[region] = image
                 else:
-                    logger.warning(f'Found image {image.name} without proper tags in region {region}')
+                    logger.warning(f'Ignoring image {image.id} ({image.name}) without proper tags in region {region}')
+                    for bdm in image.extra['block_device_mapping']:
+                        snaptmp = bdm['ebs']['snapshot_id']
+                        ignored_snapshots.append(snaptmp)
 
             for snapshot in driver.list_snapshots(owner=self._info.account):
                 tags = snapshot.extra['tags']
                 family = tags.get('ImageFamily', None)
                 version = tags.get('ImageVersion', None)
+                if snapshot.id in ignored_snapshots:
+                    logger.warning(f'Snapshot {snapshot.id} is associated with ignored AMI')
+                    next
                 if family and version:
                     v = self.setdefault(family).versions.setdefault(ImageVersion.from_string(version))
                     v.snapshots[region] = snapshot

commit 1f3e633c4aaf518382ef39b107893280db256d58

Fix typo
Fixes: 5d252c3 ("ec2: ignore snapshots associated with ignored
AMIs").  I apparently forgot what language I was writing...

src/debian_cloud_images/images/ec2/s1_cloud_family.py
@@ -74,7 +74,7 @@ class StepCloudFamilies(collections.abc.Mapping):
                 version = tags.get('ImageVersion', None)
                 if snapshot.id in ignored_snapshots:
                     logger.warning(f'Snapshot {snapshot.id} is associated with ignored AMI')
-                    next
+                    continue
                 if family and version:
                     v = self.setdefault(family).versions.setdefault(ImageVersion.from_string(version))
                     v.snapshots[region] = snapshot