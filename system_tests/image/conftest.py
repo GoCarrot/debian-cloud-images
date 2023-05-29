d1e0a4ed5faea6e311158134845fc1e05928c48f
Bastian Blank <waldi@debian.org>
2023-05-29 23:43:28 +0200

    Generate test fixtures more

--- system_tests/image/conftest.py
+++ system_tests/image/conftest.py
@@ -1,29 +1,53 @@
 import pytest
 
 import collections
+import typing
+
+
+@pytest.fixture(autouse=True)
+def raise_test(request) -> None:
+    if not hasattr(request, 'param'):
+        return
+
+    raise request.param
+
+
+class Fixtures:
+    def __init__(self) -> None:
+        self.__func = {}
+
+    def parametrize(self, name: str, metafunc: typing.Any) -> None:
+        if func := self.__func.get(name):
+            try:
+                metafunc.parametrize(name, func(metafunc))
+            except BaseException as e:
+                metafunc.parametrize('raise_test', [e], indirect=True)
+
+    def register(self, f: typing.Any) -> None:
+        self.__func[f.__name__] = f
+
+
+_fixtures = Fixtures()
 
 
 PasswdEntry = collections.namedtuple('PasswdEntry', ['name', 'passwd', 'uid', 'gid', 'gecos', 'dir', 'shell'])
 
 
-@pytest.fixture(scope="session")
-def image_passwd_entry():
-    pytest.fail('Unable to read /etc/passwd', pytrace=False)
-
-
 # Read infos from /etc/passwd as it apears in the image and create entries as
 # test parameters
-def _read_image_passwd_entry(metafunc):
+@_fixtures.register
+def image_passwd_entry(metafunc):
     path = metafunc.config.getoption('mount_path') / 'etc' / 'passwd'
     if path.exists():
-        params = []
         with path.open() as f:
+            params = []
             for line in f.readlines():
                 e = PasswdEntry(*line.strip().split(':'))
                 params.append(pytest.param(e, id=e.name))
-        metafunc.parametrize('image_passwd_entry', params)
+            return params
+    pytest.fail('Unable to read /etc/passwd inside image mount', pytrace=False)
 
 
 def pytest_generate_tests(metafunc):
-    if 'image_passwd_entry' in metafunc.fixturenames:
-        _read_image_passwd_entry(metafunc)
+    for fixturename in metafunc.fixturenames:
+        _fixtures.parametrize(fixturename, metafunc)
